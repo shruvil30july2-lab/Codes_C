14e30e5f6813e6d9f1189af4565f74a9cc6494c5
Shruvil Shrivastava <shruvil30july2@gmail.com>
2026-02-17 14:47:37 +0530

    Refactor quadratic equation solver with roots calculation

--- Quadratic Equation.c
+++ Quadratic Equation.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
+#include <math.h>
 int main() {
-    float a, b, c, d;
-
-    printf("Enter a, b and c: ");
-    scanf("%f %f %f", &a, &b, &c);
-
-    if (a == 0) {
-        printf("Not a quadratic equation\n");
-    } else {
-        d = (b * b) - (4 * a * c);
-
-        if (d > 0)
-            printf("Two distinct real roots\n");
-        else if (d == 0)
-            printf("One real root\n");
-        else
-            printf("No real roots\n");
+    float a,b,c,d,r1,r2;
+    printf("Enter a,b,c for ax^2+bx+c=0: ");
+    scanf("%f %f %f",&a,&b,&c);
+    d=(pow(b,2)-(4*a*c));
+    if (d>0){
+        r1=(-b+sqrt(d))/(2*a);
+        r2=(-b-sqrt(d))/(2*a);
+        printf("%.2f and %.2f are the roots",r1,r2);
     }
-
+    if (d==0){
+        r1=(-b)/(2*a);
+        printf("%.2f is the only root",r1);
+    }
+    if (d<0){
+        printf("Non real roots");
+    }
     return 0;
 }
